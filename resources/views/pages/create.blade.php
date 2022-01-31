b2bc4e5bacd9b96f1dd610a1f46fc86083848a03
Riccardo <riccardolipparini0@gmail.com>
2022-01-31 20:41:43 +0100

    edit validate

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.main-layouts')
 @section('content')
 
-<h1>hello world</h1>
+<h1>Aggiungi nuovo film</h1>
+
+@if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+@endif
 
 <form action="{{ route('store')}}" method="POST">
 
